3e198bd04d5fd254f0b7fb1336bdaa6665442fff
Baodong <baodong.cao@openmart.com>
2026-01-29 10:08:29 +0000

    Fix path

--- example/infer_tts.sh
+++ example/infer_tts.sh
@@ -6,7 +6,7 @@ echo "PYTHONPATH set to: $PYTHONPATH"
 
 text=${1:-"hmm"}
 prompt_text=${2:-"喜欢攀岩、徒步、滑雪的语言爱好者，以及过两天要带着全部家当去景德镇做陶瓷的白日梦想家。"}
-prompt_audio=${3:-"example/audios/female_mandarin.wav"}
+prompt_audio=${3:-"/workspace/SoulX-Podcast/example/audios/female_mandarin.wav"}
 output_path=${4:-"outputs/mandarin_tts.wav"}
 
 
@@ -28,24 +28,24 @@ output_path=${3:-"outputs/infer_tts.wav"}
 case "${voice_id}" in
     "1")
         prompt_text="So, streaming surpasses combined broadcast and cable viewing for the first time ever."
-        prompt_audio="example/audios/S1.wav"
+        prompt_audio="/workspace/SoulX-Podcast/example/audios/S1.wav"
         ;;
     "2")
         prompt_text="I had somebody spend over 7 million against me in that race, it was tough."
-        prompt_audio="example/audios/S2.wav"
+        prompt_audio="/workspace/SoulX-Podcast/example/audios/S2.wav"
         ;;
     "3")
         prompt_text="喜欢攀岩、徒步、滑雪的语言爱好者，以及过两天要带着全部家当去景德镇做陶瓷的白日梦想家。"
-        prompt_audio="example/audios/female_mandarin.wav"
+        prompt_audio="/workspace/SoulX-Podcast/example/audios/female_mandarin.wav"
         ;;
     "4")
         prompt_text="呃，还有一个就是要跟大家纠正一点，就是我们在看电影的时候，尤其是游戏玩家，看电影的时候，在看到那个到西北那边的这个陕北民谣，嗯，这个可能在想，哎，是不是他是受到了黑神话的启发？"
-        prompt_audio="example/audios/male_mandarin.wav"
+        prompt_audio="/workspace/SoulX-Podcast/example/audios/male_mandarin.wav"
         ;;
     *)
         # 默认使用第一组
         prompt_text="喜欢攀岩、徒步、滑雪的语言爱好者，以及过两天要带着全部家当去景德镇做陶瓷的白日梦想家。"
-        prompt_audio="example/audios/female_mandarin.wav"
+        prompt_audio="/workspace/SoulX-Podcast/example/audios/female_mandarin.wav"
         ;;
 esac
 
