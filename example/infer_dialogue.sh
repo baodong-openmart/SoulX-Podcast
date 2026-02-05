5301dceebacd9755321229bca43007a80dbb1558
baodong-openmart <baodong.cao@openmart.com>
2026-02-05 10:12:15 +0700

    Update infer_dialogue.sh

--- example/infer_dialogue.sh
+++ example/infer_dialogue.sh
@@ -1,14 +1,9 @@
-export PYTHONPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)"
-echo "PYTHONPATH set to: $PYTHONPATH"
-
-# Note: To infer Chinese dialects, set model_dir to "pretrained_models/SoulX-Podcast-1.7B-dialect
-model_dir=pretrained_models/SoulX-Podcast-1.7B
 #input_file=/workspace/SoulX-Podcast/example/podcast_script/script_english.json
 input_file=/workspace/rhythm-emulation/assets/output/podcast_script.json
 echo ${input_file}
 
-/usr/bin/python /workspace/SoulX-Podcast/cli/podcast.py \
+uv run cli/podcast.py \
         --json_path ${input_file} \
-        --model_path ${model_dir} \
+        --model_path "pretrained_models/SoulX-Podcast-1.7B" \
         --output_path /workspace/rhythm-emulation/assets/output/podcast-with-pi.wav \
         --seed 7
